1b8ada3c80ad874467e6a7a4e3355eb186c13197
Isaac Benitez <ibsandoval@gmail.com>
2021-10-30 13:06:29 -0700

    Add support for heroku dynamic port.

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(443);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
 
 app.get('/', (req, res) => {
     res.json({"msg":"Server up..."})
