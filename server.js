b882265fe9931919fbc70c55a4cf1a311f6ed51c
Isaac Benitez <ibsandoval@gmail.com>
2021-10-31 14:37:24 -0700

    Add insufficient funds simulation.

--- server.js
+++ server.js
@@ -13,11 +13,20 @@ app.get('/', (req, res) => {
 
 app.post('/auth', (req, res) => {
 
+    let code = "00";
+
+    // simulate insufficient funds
+    if (req.body.amount === 404) {
+        code = "51";
+    }
+
     const auth_response = {
         "authorization_id": req.body.authorization_id,
-        "response_code": "00",
+        "response_code": code,
         "auth_code": Math.random().toString(36).substr(2, 6).toUpperCase()
     }
+
+    console.log(auth_response);
 
     res.json(auth_response);
 })
